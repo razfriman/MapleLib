b9b1619a6a5b5f914924f82bfb20cb0b700df45a
Raz Friman <superraz375@yahoo.com>
2014-06-06 04:49:17 -0500

    Initial Upload. Migrated from GoogleCode

---
+++ MapleLib/PacketLib/AbstractPacket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MapleLib.PacketLib
+{
+	public abstract class AbstractPacket
+	{
+		protected MemoryStream mBuffer;
+
+		public byte[] ToArray()
+		{
+			return mBuffer.ToArray();
+		}
+	}
+}
---
+++ MapleLib/PacketLib/SessionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapleLib.PacketLib
+{
+	public enum SessionType
+	{
+		SERVER_TO_CLIENT,
+		CLIENT_TO_SERVER
+	}
+}
